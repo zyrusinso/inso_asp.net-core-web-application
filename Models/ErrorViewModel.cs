1257c015fba9a77d24ce779ceeba57d5e23aae4f
zyrus inso <zyrusinso@gmail.com>
2024-02-17 18:59:14 +0800

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace inso_zyruslee_activity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
